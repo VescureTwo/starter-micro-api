28c36dbb63183842241f33c022b06d2378bf83fe
VescureTwo <83725273+VescureTwo@users.noreply.github.com>
2023-01-01 09:39:19 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require('http');
-http.createServer(function (req, res) {
+const app = http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write(req.body);
+    res.write();
     res.end();
 }).listen(process.env.PORT || 3000);
+
+app.post('/userlogin', function(sReq, sRes){    
+    var email = sReq.body;
+    console.log(email);
+}
